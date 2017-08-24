aa7ed1aa5cf37df28e5ac0ecd6c2a635829ff10c
DanielGreene101 <d.b.greene101@gmail.com>
2017-08-24 12:51:26 -0500

    merge

--- javascripts/firebase.js
+++ javascripts/firebase.js
@@ -1,21 +1,6 @@
 'use strict';
 var firebase = require("firebase");
-const fbURL = `https://movie-list-bb8f4.firebaseio.com/`;
-var FbCreds = {
-    apiKey: "AIzaSyCdQwFnhT6W_VnKNdl5ESRwqXVXmHNjRQo",
-    authDomain: "movie-list-bb8f4.firebaseapp.com",
-    databaseURL: "https://movie-list-bb8f4.firebaseio.com",
-    projectId: "movie-list-bb8f4",
-    storageBucket: "movie-list-bb8f4.appspot.com",
-    messagingSenderId: "377715718874"
-};
-
-var config = {
-  apiKey: FbCreds.apiKey,
-  authDomain: FbCreds.authDomain,
-  databaseURL: FbCreds.databaseURL
-};
-
+var config = require('./private.js');
 firebase.initializeApp(config);
 
 var provider = new firebase.auth.GoogleAuthProvider();
@@ -23,7 +8,6 @@ console.log("provider is", provider);
 
 
 firebase.getFBsettings = function(){
-     console.log("getFBsettings", config);
      return config;
 };
 
@@ -63,19 +47,3 @@ firebase.auth().onAuthStateChanged(function(user){
 
 logInGoogle();
 
-// firebase.auth().signInWithPopup(provider).then(function(result) {
-//       // This gives you a Google Access Token. You can use it to access the Google API.
-//       var token = result.credential.accessToken;
-//       // The signed-in user info.
-//       var user = result.user;
-//       // ...
-//     }).catch(function(error) {
-//       // Handle Errors here.
-//       var errorCode = error.code;
-//       var errorMessage = error.message;
-//       // The email of the user's account used.
-//       var email = error.email;
-//       // The firebase.auth.AuthCredential type that was used.
-//       var credential = error.credential;
-//       // ...
-//     });
