ecc4ed9875f97f38f68cec0625a4123505e40059
DanielGreene101 <d.b.greene101@gmail.com>
2017-08-25 10:54:19 -0500

    working buttons

--- javascripts/event.js
+++ javascripts/event.js
@@ -1,8 +1,19 @@
 "use strict";
+var somethingElse = require('./firebase.js');
+var fb = require('firebase');
 function buttonChanges(){
 	$('#unwatched').fadeIn(2000);
 	$('#watched').fadeIn(2000);
-	// $('#loginbutton').
+	$("#loginbutton").css("display","none");
+	$('#logoutbutton').css("display", "block");
+	$(document).on("click", "#logoutbutton", ()=>{
+	$('#unwatched').hide();
+	$('#watched').hide();
+	$("#loginbutton").css("display","block");
+	$('#logoutbutton').css("display", "none");
+		 return fb.auth().signOut();
+	});
+
 }
 
 module.exports = {buttonChanges};
