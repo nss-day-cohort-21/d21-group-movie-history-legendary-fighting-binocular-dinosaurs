6daec3d09305db3b598397935c7ebe6b32389beb
Fang <fang-wen.shen@vanderbilt.edu>
2017-08-25 09:13:58 -0500

    accordion with cast list

--- javascripts/dombuilder.js
+++ javascripts/dombuilder.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var cards = require('../templates/cards.hbs');
-var requests = require("./requests.js");
+var request = require("./requests.js");
 
 
 
@@ -21,11 +21,20 @@ function domBuilder(movie){
        console.log("obj from domBuilder", movie);
      
       $(".row").html('');
-      movie.forEach((item) => {
+      movie.forEach((item,index) => {
+     console.log("castiss?", item.cast);
+       
          if (item.poster_path!==null) {
+          
+          
+            
             $(".row").append(cards(item));
+            console.log("domBuilder(items)", item);
+                   
          }
-
+          // if (item.cast !==undefined) {
+            
+          // }
          
 
       });
--- javascripts/requests.js
+++ javascripts/requests.js
@@ -5,10 +5,8 @@ var template = require('./dombuilder');
 function initialSearch(searchInput) {
 	return new Promise((resolve,reject)=>{
       
-        $.ajax({
-            
+        $.ajax({  
             url: `https://api.themoviedb.org/3/search/movie?api_key=01fccab4977fb9e675b2a37846f08da4&language=en-US&query=${searchInput}&page=1&include_adult=false`,
-        // crossDomain:true
         }).done((songdata)=>{
             resolve(songdata.results);
             console.log("songdata is", songdata.results);
@@ -18,33 +16,58 @@ function initialSearch(searchInput) {
 }
     
 
-function castSearch(poster_path) {
+function castSearch(movieid) {
         // console.log("posterpath", poster_path);
             
         return new Promise((resolve,reject)=>{
-
-            let url="https://image.tmdb.org/t/p/w500"+`${poster_path}`;
-           
+        $.ajax({
+            url:"https://api.themoviedb.org/3/movie/" +`${movieid}` + "/credits?api_key=c93dee63a7012453634a328e5dd78eef"
+         }).done((url)=>{  
             resolve(url);
                 
         });
-    }
+    });
+}
 
 
+var carddata={};
 
 $("#searchInput").on("keydown",(e)=>{
     if (e.keyCode == 13) {
         e.preventDefault();
         let search = $("#searchInput").val();
         initialSearch(search).then((data)=>{
-                template(data);
+            carddata = data;
+            data.forEach((item,index)=>{
+    
+                    
+                castSearch(item.id).then((castid)=>{
+                    if (castid.cast.length>0) {
+                        carddata[index].cast = castid.cast;
+                    }   
+                    console.log("carddata from first", carddata[index].cast);
+                    if (carddata[index].cast!==undefined ) {
+                        console.log("got here!!!!!!");
+                            
+                        for (var i = 0; i<5;i++) {
+                            console.log("cast members", carddata[index].cast[i]);
+                              template(carddata);  
+                        }
+
+                    }
+                });
+                    
+                    
+            });
+                
+                
         });
     }
 });
 	
 
 
-module.exports ={initialSearch};
+module.exports =castSearch;
 
 
 
